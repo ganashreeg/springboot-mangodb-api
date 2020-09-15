781d8e73bc14dced46034f01b105954bd79d9233
Ganashree G <ganashreeg@gmail.com>
2020-09-15 15:44:44 +0530

    spring boot project initialization

---
+++ src/main/java/com/ogmatech/springbootmangodbapi/SpringbootMangodbApiApplication.java
@@ -0,0 +1,13 @@
+package com.ogmatech.springbootmangodbapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMangodbApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMangodbApiApplication.class, args);
+	}
+
+}
